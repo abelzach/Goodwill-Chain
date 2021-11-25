0c223caaccf2c0cdaf3f3c1507200c988bcde6b8
UltimateRoman <59477486+UltimateRoman@users.noreply.github.com>
2021-11-25 22:54:29 +0530

    Implemented lazy minting

--- contracts/GoodwillChain.sol
+++ contracts/GoodwillChain.sol
@@ -3,9 +3,8 @@ pragma solidity >=0.4.22 <0.9.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 
-contract GoodwillChain is ERC721, IERC721Receiver, Ownable {
+contract GoodwillChain is ERC721, Ownable {
 
   mapping(address => string) artists;
 
@@ -15,10 +14,9 @@ contract GoodwillChain is ERC721, IERC721Receiver, Ownable {
 
   mapping(uint => NFT) public nfts;
 
-
   struct NFT {
     uint id;
-    uint256 price;
+    uint price;
     bool isListed;
     string name;
     string filecid;
@@ -69,29 +67,18 @@ contract GoodwillChain is ERC721, IERC721Receiver, Ownable {
   function createNFT(string memory _name,string memory _filecid, uint256 _price) external {
     require(bytes(_name).length > 0 && bytes(_filecid).length > 0, "Name and file required");
     tCount++;
-    _safeMint(address(this), tCount);
-    _setTokenURI(tCount, _filecid);
     nfts[tCount] = NFT(tCount, _price, true, _name, _filecid, artists[msg.sender], msg.sender, msg.sender);
     emit createdNFT(tCount, nfts[tCount].name, nfts[tCount].owner);
   }
 
   function buyNFT(uint _id) external payable idExists(_id) {
+    require(msg.value >= nfts[_id].price, "Insufficient amount");
     org_address.transfer(msg.value);
     funds = funds + msg.value;
-    safeTransferFrom(address(this), msg.sender, _id);
+    _safeMint(msg.sender, _id);
+    _setTokenURI(_id, nfts[_id].filecid);
     nfts[_id].owner = msg.sender;
     nfts[_id].isListed = false;
     emit boughtNFT(_id, nfts[_id].owner);
   }
-
-  function onERC721Received(
-    address,
-    address,
-    uint256,
-    bytes calldata
-  ) external override returns(bytes4) {
-      return bytes4(keccak256("onERC721Received(address,address,uint256,bytes)"));
-  }
-
-  receive() external payable {}
 }
